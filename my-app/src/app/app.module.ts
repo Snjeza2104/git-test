87297acaa744f3a2b9318ec56898c5190ae66b74
Snjeza2104 <snjeza.mijosevic@gmail.com>
2020-05-14 20:51:09 +0200

    Components part 2

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,17 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar'; 
-import { FlexLayoutModule } from '@angular/flex-layout';
+
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 
-import { MatListModule } from '@angular/material/list';
+
 
 
 @NgModule({
@@ -25,7 +30,10 @@ import { MatListModule } from '@angular/material/list';
     BrowserAnimationsModule,
     MatToolbarModule,
     FlexLayoutModule,
-    MatListModule
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- my-app/src/app/menu/menu.component.ts
+++ my-app/src/app/menu/menu.component.ts
@@ -1,14 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
 
-@Component({
-  selector: 'app-menu',
-  templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
-})
-export class MenuComponent implements OnInit {
-	 dishes: Dish[] = [
-    {
+const DISHES: Dish[] = [
+ {
       id: '0',
       name: 'Uthappizza',
       image: '/assets/images/uthappizza.png',
@@ -49,7 +43,19 @@ export class MenuComponent implements OnInit {
       price: '2.99',
       description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
     }
-   ];
+ 
+ ];
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+   dishes = DISHES;
+
+  selectedDish = DISHES[0];
+    
   constructor() { }
 
   ngOnInit(): void {
